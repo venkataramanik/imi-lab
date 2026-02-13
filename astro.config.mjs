6a21ca31cc8bb0ddb73e3fceb444cd7e331b8be1
venkataramanik <35788210+venkataramanik@users.noreply.github.com>
2026-02-13 15:57:20 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://imi-lab.net',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
   output: 'static'
 });
